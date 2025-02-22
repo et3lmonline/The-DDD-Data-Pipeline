2b086bb8852af7a843f1eb7574a3c8521dbe873d
Al-Moatasem <accmmsherif@gmail.com>
2025-02-22 22:21:17 +0200

    using local filesystem as destination - csv files

--- etl/pipelines/incremental_load_local_file_system.py
+++ etl/pipelines/incremental_load_local_file_system.py
@@ -1,6 +1,7 @@
 import os
 
 import dlt
+from dlt.destinations import filesystem
 from dlt.sources.sql_database import sql_database
 
 database = os.environ.get("SRC_PG_CONTOSO_DATABASE")
@@ -21,16 +22,22 @@ my_tables_data = sql_database(
 
 
 # 02 : Destination
-
+buckets_root_dir = os.environ.get("OUTPUTS_DIR")
+bucket_url = f"file:///{buckets_root_dir}"
+local_filesystem_destination = filesystem(bucket_url=bucket_url)
 
 # 03 Pipeline
 destination_schema_name = "contoso_raw"
 pipeline = dlt.pipeline(
     pipeline_name="my_first_dlt_pipeline",
-    destination="duckdb",
+    destination=local_filesystem_destination,
     dataset_name=destination_schema_name,
     progress="log",
 )
 
 # 04: run the pipeline
-pipeline.run(data=my_tables_data, write_disposition="replace")
+pipeline.run(
+    data=my_tables_data,
+    write_disposition="append",
+    loader_file_format="csv",
+)
